53b5395837bbfa4b85c19c893d5293a3272720a2
Vilstig <fj.janas@gmail.com>
2025-04-03 11:36:25 +0200

    Added full functionality for task 5. Added test_image dir

--- media_convert.py
+++ media_convert.py
@@ -3,10 +3,11 @@ import sys
 import subprocess
 import datetime
 import json
-from utils import find_media_files, get_output_dir, conversion_log, extensions
+from utils import find_media_files, get_output_dir, conversion_log, MEDIA_EXTENSIONS, IMAGE_EXTENSIONS, is_image
+
 
 def convert_file(input_path, output_format):
-    if output_format not in extensions:
+    if output_format not in MEDIA_EXTENSIONS + IMAGE_EXTENSIONS:
         print(f'Error: {output_format} is not a supported extension')
         sys.exit(1)
     timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
@@ -14,10 +15,18 @@ def convert_file(input_path, output_format):
     output_dir = get_output_dir()
     output_path = os.path.join(output_dir, f'{timestamp}-{orig_name}.{output_format}')
 
+
     try:
-        subprocess.run(['ffmpeg', '-i', input_path, output_path, '-y'], check=True, capture_output=True)
-        conversion_log(timestamp, input_path, output_format, output_path)
+        if is_image(input_path):
+            tool_used = "ImageMagick"
+            subprocess.run(['magick', input_path, output_path], check=True, capture_output=True)
+        else:
+            tool_used = "FFmpeg"
+            subprocess.run(['ffmpeg', '-i', input_path, output_path, '-y'], check=True, capture_output=True)
+
+        conversion_log(timestamp, input_path, output_format, output_path, tool_used)
         print(f'Conversion ended successfully')
+
     except subprocess.CalledProcessError:
         print(f'Failed converting file: {input_path}')
 
--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-import os
 import json
-import datetime
+import os
 
-extensions = ('mp4', 'avi', 'mov', 'mkv', 'mp3', 'wav', 'flac')
+MEDIA_EXTENSIONS = ('.mp4', '.avi', '.mov', '.mkv', '.mp3', '.wav', '.flac')
+IMAGE_EXTENSIONS = ('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'tif', 'webp', 'ico', 'svg', 'heic')
 
 def find_media_files(dir_path):
     file_list = []
 
     for root, _, files in os.walk(dir_path):
         for file in files:
-            if file.lower().endswith(extensions):
+            if file.lower().endswith(MEDIA_EXTENSIONS + IMAGE_EXTENSIONS):
                 file_list.append(os.path.join(root, file))
 
     return file_list
@@ -17,14 +17,18 @@ def find_media_files(dir_path):
 def get_output_dir():
     return os.getenv("CONVERTED_DIR", os.path.join(os.getcwd(), 'converted_files'))
 
-def conversion_log(timestamp, input_file, output_ext, output_file):
+def is_image(file_path):
+    return file_path.lower().endswith(IMAGE_EXTENSIONS)
+
+def conversion_log(timestamp, input_file, output_ext, output_file, tool_used):
     log_file = os.path.join(get_output_dir(), 'history.json')
 
     entry = {
         'timestamp': timestamp,
         'input_file': input_file,
         'output_ext': output_ext,
-        'output_file': output_file
+        'output_file': output_file,
+        'tool_used': tool_used
     }
 
     if os.path.exists(log_file):
